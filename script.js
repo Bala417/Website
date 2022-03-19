9fec80a4ff17f2c7137f6d94c95330047bac27d8
Bala417 <33387172+Bala417@users.noreply.github.com>
2022-03-19 23:00:31 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function welcomemsg() {
+    return (alert("Welcome"),
+        alert(details.checkDrL())
+    )
+}
+
+function closebtn() {
+    document.querySelector(".title").style.visibility = "hidden"
+}
